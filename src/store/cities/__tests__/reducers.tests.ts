497474a7bf14207cbf8012703dd6354fcb52f0aa
Wiktor Musial <wiktormusial@icloud.com>
2022-03-10 22:13:32 +0100

    Create addCity func

---
+++ src/store/cities/__tests__/reducers.tests.ts
@@ -0,0 +1,15 @@
+import reducer, { addCity } from "../citiesSlice";
+
+it("on init Initialstate is empty", () => {
+  expect(reducer(undefined, {})).toEqual({
+    cities: [],
+  });
+});
+
+it("addCity is adding city", () => {
+  expect(
+    reducer(undefined, addCity({ city: "Katowice", temp: 15 }))
+  ).toEqual({
+    cities: [{ city: "Katowice", temp: 15 }],
+  });
+});
--- src/store/cities/citiesSlice.ts
+++ src/store/cities/citiesSlice.ts
@@ -1,18 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "../store";
 
-interface State {
+interface Cities {
   city: string;
   temp: number;
 }
 
-const initialState: Array<State> | [] = [];
+interface State {
+  cities: Cities[];
+}
+
+const initialState = {
+  cities: [],
+} as State;
 
 const citiesSlice = createSlice({
   name: "cities",
   initialState,
-  reducers: {},
+  reducers: {
+    addCity: (state, action: PayloadAction<Cities>) => {
+      state.cities.push(action.payload);
+    },
+  },
 });
+
+export const { addCity } = citiesSlice.actions
 
 export const selectCities = (state: RootState) => state.cities;
 
