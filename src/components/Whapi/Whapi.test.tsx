8225516513eec286f7f5c97e080e481359f5745b
Wiktor Musial <wiktormusial@icloud.com>
2022-03-10 23:09:01 +0100

    Fix Test

--- src/components/Whapi/Whapi.test.tsx
+++ src/components/Whapi/Whapi.test.tsx
@@ -1,7 +1,10 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from 'react-redux'
+import { store } from '../../store/store'
 import Whapi from "./index";
 
+
 it("renders one input", () => {
-  render(<Whapi />);
+  render(<Provider store={store}><Whapi /></Provider>);
   expect(screen.getByTestId("search-bar"));
 });
